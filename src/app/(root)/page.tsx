ba6d6db247e525ccc4641dd302deedf21e1bdcd1
Mochammad Fauzan Fadilah <ozan.it212@gmail.com>
2025-03-16 18:00:23 +0700

    Add assets, components, and configuration for Prostore E-Commerce App

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,13 @@
+
+
+const HomePage = () => {
+    return (
+        <>
+        <div className="text-blue-300">
+            Prostore E-Commerce App
+        </div>
+        </>
+     );
+}
+
+export default HomePage;
---
+++ src/lib/constants/index.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Prostore';
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'Prostore - Your one-stop solution for all your needs';
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:2122';
