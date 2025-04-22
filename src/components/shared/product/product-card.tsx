3b052cdf966558916c3093fc125304989a9c7870
masozan7 <fauzan.fadilah@epfootwear.com>
2025-04-22 08:53:01 +0700

    Refactor ProductCard and ProductList components to use strong typing for props and improve code readability

--- src/components/shared/product/product-card.tsx
+++ src/components/shared/product/product-card.tsx
@@ -2,29 +2,50 @@ import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import Image from "next/image";
 import Link from "next/link";
 
-const ProductCard = ({ product}:{product:any;}) => {
+type Product = {
+      id: number;
+      name: string;
+      slug: string;
+      category: string;
+      description: string;
+      images: string[];
+      price: number;
+      brand: string;
+      rating: number;
+      numReviews: number;
+      stock: number;
+      isFeatured: boolean;
+      banner: string;
+};
+
+type ProductCardProps = {
+      product: Product;
+};
+
+const ProductCard = ({ product }: ProductCardProps) => {
       return (
             <Card className="w-full max-w-sm">
                   <CardHeader className="p-0 items-center">
                         <Link href={`/product/${product.slug}`} className="flex flex-col items-center">
-                        <Image src={product.images[0]} alt={product.name} width={300} height={300} priority={true} className="object-cover" />
+                              <Image
+                                    src={product.images[0]}
+                                    alt={product.name}
+                                    width={300}
+                                    height={300}
+                                    priority={true}
+                                    className="object-cover"
+                              />
                         </Link>
                   </CardHeader>
                   <CardContent className="p-4 grid gap-4">
-                        <div className="text-xs">
-                              {product.brand}
-                        </div>
+                        <div className="text-xs">{product.brand}</div>
                         <Link href={`/product/${product.slug}`}>
-                              <h2 className="text-sm font-medium">
-                                    {product.name}
-                              </h2>
+                              <h2 className="text-sm font-medium">{product.name}</h2>
                         </Link>
-                        <div className="flex-between gap-4">
-                              <p>{product.rating} Starts</p>
+                        <div className="flex justify-between gap-4">
+                              <p>{product.rating} Stars</p>
                               {product.stock > 0 ? (
-                                    <p className="font-bold">
-                                          {product.price}
-                                    </p>
+                                    <p className="font-bold">${product.price.toFixed(2)}</p>
                               ) : (
                                     <p className="text-destructive">Out Of Stock</p>
                               )}
@@ -32,5 +53,6 @@ const ProductCard = ({ product}:{product:any;}) => {
                   </CardContent>
             </Card>
       );
-}
+};
+
 export default ProductCard;
--- src/components/shared/product/product-list.tsx
+++ src/components/shared/product/product-list.tsx
@@ -1,24 +1,46 @@
 import ProductCard from "./product-card";
 
-const ProductList = ({ data, title, limit}: {data: { id: number; name: string }[], title?: string, limit?:number})=>{
+type Product = {
+      id: number;
+      name: string;
+      slug: string;
+      category: string;
+      description: string;
+      images: string[];
+      price: number;
+      brand: string;
+      rating: number;
+      numReviews: number;
+      stock: number;
+      isFeatured: boolean;
+      banner: string;
+};
+
+type ProductListProps = {
+      data: Product[];
+      title?: string;
+      limit?: number;
+};
+
+const ProductList = ({ data, title, limit }: ProductListProps) => {
       const limitedData = limit ? data.slice(0, limit) : data;
+
       return (
             <div className="my-10">
-                  <h2 className="h2-bold mb-4" >
-                        {title}
-                  </h2>
+                  {title && <h2 className="h2-bold mb-4">{title}</h2>}
                   {data.length > 0 ? (
                         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-                              {limitedData.map((product:any) => (
-                                 <ProductCard key={product.slug} product={product} />
+                              {limitedData.map((product) => (
+                                    <ProductCard key={product.slug} product={product} />
                               ))}
                         </div>
-                  ):(
+                  ) : (
                         <div>
-                        <p>No product found</p>
+                              <p>No products found</p>
                         </div>
                   )}
             </div>
       );
-}
+};
+
 export default ProductList;
