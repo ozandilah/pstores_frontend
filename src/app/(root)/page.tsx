4c01bd12503dad0ac288e2912765a684f429bb5c
masozan7 <fauzan.fadilah@epfootwear.com>
2025-04-24 15:02:39 +0700

    Add functionality to fetch latest products from the database and update homepage; refactor product card display

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,16 +1,18 @@
 import ProductList from "@/components/shared/product/product-list";
-import sampleData from "@/db/sample-data";
+import { getLatestProducts } from "@/lib/actions/product.actions";
 
-const HomePage = () => {
+const  HomePage = async () => {
+  const latestProducts = await getLatestProducts();
+
   // Transformasi data untuk memberikan fallback pada banner
-  const products = sampleData?.products.map((product) => ({
-    ...product,
-    banner: product.banner || "/fallback-banner.jpg", // Fallback jika banner null
-  })) || [];
+  // const products = sampleData?.products.map((product) => ({
+  //   ...product,
+  //   banner: product.banner || "/fallback-banner.jpg", // Fallback jika banner null
+  // })) || [];
 
   return (
     <div className="container mx-auto p-4">
-      <ProductList data={products} title="Featured Products" limit={4} />
+      <ProductList data={latestProducts} title="Featured Products" />
     </div>
   );
 };
--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -1,3 +1,4 @@
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Prostore';
 export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'Prostore - Your one-stop solution for all your needs';
 export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:2122';
+export const LATEST_PRODUCTS_LIMIT = Number(process.env.LATEST_PRODUCTS_LIMIT) || 4;
