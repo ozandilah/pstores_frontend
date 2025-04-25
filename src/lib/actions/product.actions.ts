dc95ad191f2467c80494691bddd5ed0f1ac284d7
masozan7 <fauzan.fadilah@epfootwear.com>
2025-04-25 16:41:35 +0700

    Integrate Neon database support with Prisma; add WebSocket connection handling and extend PrismaClient for custom result transformation

--- src/lib/actions/product.actions.ts
+++ src/lib/actions/product.actions.ts
@@ -1,13 +1,14 @@
 // filepath: /E:/PERSONAL/pstore/src/lib/actions/product.actions.ts
 'use server';
-import { PrismaClient } from "@prisma/client";
 import { convertToPlainObject } from "../utils";
 import { LATEST_PRODUCTS_LIMIT } from "../constants";
+import { prisma } from "@/db/prisma";
 
-const prisma = new PrismaClient(); // Reuse a single PrismaClient instance
 
 // Get latest products
 export async function getLatestProducts() {
+  
+  
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
     orderBy: {
